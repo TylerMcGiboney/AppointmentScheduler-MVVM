1a812196142af55a3254cb5f6a06c4f2cb28e4f2
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-05 18:26:48 -0700

    chore: Created RelayCommand

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Appointment_Scheduler.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+        public bool CanExecute(Object e)
+        {
+            return true;
+        }
+
+        public void Execute(Object e)
+        {
+            _execute(e);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
